5f6d5e63f7e02aa31401929ad26789df7faf8e46
unknown <carlosarroyoam@gmail.com>
2022-05-26 16:08:02 -0500

    add book repository implementation

--- src/main/java/com/carlosarroyoam/library/service/BookService.java
+++ src/main/java/com/carlosarroyoam/library/service/BookService.java
@@ -7,7 +7,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.NotFoundException;
 
-import com.carlosarroyoam.library.model.Author;
 import com.carlosarroyoam.library.model.Book;
 import com.carlosarroyoam.library.repository.BookRepository;
 
@@ -37,22 +36,21 @@ public class BookService {
 	}
 
 	public Book store(Book book) {
-		Optional<Book> bookByIsbn = Optional.ofNullable(
-				new Book("2", "Mastering Java EE 8 Application Development", new Author(1L, "Kapila Bogahapitiya")));
-
-		return bookByIsbn.get();
+		return new Book();
 	}
 
 	public Book update(Book book, String isbn) {
-		Optional<Book> bookByIsbn = Optional.ofNullable(
-				new Book(isbn, "Mastering Java EE 8 Application Development", new Author(1L, "Kapila Bogahapitiya")));
+		Optional<Book> bookByIsbn = bookRepository.findByISBN(isbn);
+
+		if (!bookByIsbn.isPresent()) {
+			throw new NotFoundException(String.format("Book with isbn: %s was not found", isbn));
+		}
 
 		return bookByIsbn.get();
 	}
 
 	public void deleteByIsbn(String isbn) {
-		Optional<Book> bookByIsbn = Optional.ofNullable(
-				new Book(isbn, "Mastering Java EE 8 Application Development", new Author(1L, "Kapila Bogahapitiya")));
+		Optional<Book> bookByIsbn = bookRepository.findByISBN(isbn);
 
 		if (!bookByIsbn.isPresent()) {
 			throw new NotFoundException(String.format("Book with isbn: %s was not found", isbn));
