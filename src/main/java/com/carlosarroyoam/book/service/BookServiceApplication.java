522193e61299d2c9160cdc1019f73391bf697944
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2024-02-11 17:37:09 -0600

    Actualiza context path

--- src/main/java/com/carlosarroyoam/book/service/BookServiceApplication.java
+++ src/main/java/com/carlosarroyoam/book/service/BookServiceApplication.java
@@ -3,6 +3,6 @@ package com.carlosarroyoam.book.service;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("api")
+@ApplicationPath("/api/v1")
 public class BookServiceApplication extends Application {
 }
